4a5621ff58d82b29f0887a9684c8aae1af5a2267
andriikravchuk <andrykrav9323@yandex.ru>
2016-11-30 18:27:36 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,27 @@
 <?php
 
-$dblocation = "testotakoi-testjavascript.rhcloud.com";
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 
-$dbname = "testotakoi";
+$dbname = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbuser = constant("DB_USER"); // Mysql username 
+$dbpasswd = constant("DB_PASS"); // Mysql password 
+$dbname = constant("DB_NAME"); // Database name 
 
-$dbuser = "adminSjABdQz";
+$dblocation = "http://testotakoi-testjavascript.rhcloud.com";
 
-$dbpasswd = "LuWCXbEvX-Da";
 
-$dbcnx = mysql_connect($dblocation,$dbuser,$dbpasswd);
+$dbcnx = mysql_connect(
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    getenv('OPENSHIFT_MYSQL_DB_PORT')
+);
 if (!$dbcnx)
 {
     echo( "<P>В настоящий момент сервер базы данных не доступен, поэтому корректное
